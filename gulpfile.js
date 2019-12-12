790a8ebc0bb8e8f050bb0fd1d0029ef949b31136
Evan 🌟 Chiu <evan@evanchiu.com>
2019-12-11 23:41:30 -0500

    Bumping to runtime nodejs12.x

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,10 @@ gulp.task('zip', function() {
   const paths = [
     'src/*.js'
   ];
-  gulp.src(paths, { base: '.' })
+  return gulp.src(paths, { base: '.' })
     .pipe(zip('replicator.zip'))
     .pipe(gulp.dest('build'));
 });
 
 // default runs lint, then packages the code into the build directory
-gulp.task('default', ['lint', 'zip']);
+gulp.task('default', gulp.series('lint', 'zip'));
